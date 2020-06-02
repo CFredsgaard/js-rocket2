f20ebf909db5c95308ad2bb0bbe1cacab33402a2
CFredsgaard <63876775+CFredsgaard@users.noreply.github.com>
2020-06-02 20:58:28 +0200

    explanations added
    about variables (var) and functions

--- explanations.js
+++ explanations.js
@@ -19,3 +19,35 @@ var cecilieObject = {
 
 //JS knows these things and they can be reached with . notation like this:
 cecilieObject.lastName //This needs to be done in chrome console to see anything returned
+
+
+//Explanations - Functions
+
+//A function is something that can be reused, it is much like a variable and can actually be stored inside a variable or a variable can refer to a function 
+function NAME () {} //these () are what makes it a function and the stuff that happens inside the function (called the method) is written between these {}
+
+//Functions can be written in two ways: 
+function whatIsMyFavColor () {}
+var whatIsMyFavColor = function () {} //This way there are only variables, but the functions are different from the other variables since they have their ()
+
+var doSomething = function () {
+    console.log("Do Something!!!")
+} //This doesn't return anything, it just deos something, it says undefined in the web browsers console
+
+var whatIsMyFavColor = function () {
+    return 'blue';
+} //This one is defined and it returns blue
+
+var whatIsMyFavColor = function () {
+    return true;
+
+    return 3;
+    
+    return 'blue';
+} //Anything after return doesn't run, therefor in the above example the only information we get out is: true
+
+
+var area = function (width, height) {
+    return width*height
+}
+
